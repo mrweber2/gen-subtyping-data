5963285be05ef66dbbf427d89a835cb75389eda9
Matthew Weber <mrweber2@illinois.edu>
2016-11-09 13:46:51 -0600

    initial commit

---
+++ scripts/generate_indelCSV.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+
+#
+#       Generate Features CSV for Weka
+#       Matthew Weber
+#       10/26/2016
+#
+
+import sys
+import os
+import re
+import argparse
+import numpy as np
+
+#################################################
+#       Open TSV for reading, CSV for writing   #
+#################################################
+
+parser = argparse.ArgumentParser(description='generate_FeatureCSV.py')
+parser.add_argument('-m',  type=str,        required=True,   metavar='<str>',                    help="* mutations.tsv")
+parser.add_argument('-I',                   required=False,  action='store_true', default=False, help="only indels")
+parser.add_argument('-o',  type=str,        required=True,   metavar='<str>',                    help="* output.csv")
+parser.add_argument('-c',  type=str, required=True,          metavar='<str>',                    help="tumor site")
+args = parser.parse_args()
+(TSV, OnlyIND, OUT, SITE) = (args.m, args.I, args.o, args.c)
+
+SITE = str(SITE)
+
+print 'reading input variants...'
+f = open(TSV, 'r')
+isFirst = True
+
+o = open(OUT, 'w')
+o.write('m_id,d_id,indel_length,cancer_type')
+o.write('\n')
+o.close()
+
+o = open(OUT, 'a')
+
+features = 4*[0]
+
+for line in f:
+
+	if line[0] == '#':
+		continued
+
+	if isFirst:
+		splt = line.strip().split('\t')
+		(chr) = (splt.index('chromosome'))
+		(m1,m2,m3) = (splt.index('reference_genome_allele'),splt.index('mutated_from_allele'),splt.index('mutated_to_allele'))
+		(d_id, m_id, qscore, conseq) = (splt.index('icgc_donor_id'),splt.index('icgc_mutation_id'),splt.index('quality_score'),splt.index('consequence_type'))
+
+		isFirst = False
+		#exit(1)
+		continue
+
+
+	splt = line.strip().split('\t')
+
+	[allele_ref,allele_normal,allele_tumor] = [splt[m1].upper(),splt[m2].upper(),splt[m3].upper()]
+	mut     = splt[m_id]
+	donor   = splt[d_id]
+
+	if ('-' not in allele_normal and '-' not in allele_tumor) and (len(allele_normal) > 1 or len(allele_tumor) > 1):
+		print 'skipping a complex variant...'
+		continue
+
+
+	if '-' in allele_normal:
+		len_normal = 0
+	else:
+		len_normal = len(allele_normal)
+	if '-' in allele_tumor:
+		len_tumor = 0
+	else:
+		len_tumor = len(allele_tumor)
+
+	if OnlyIND:
+
+		if len_normal != len_tumor:
+			indel_len = len_tumor - len_normal
+
+			features[0] = mut
+			features[1] = donor
+			features[2] = indel_len
+			features[3] = SITE
+			for n in features:
+				if n != SITE:
+					o.write("%s," % n)
+				else:
+					o.write("%s" % n)
+			o.write("\n")
+
+		else:
+			continue
+		features = 4*[0]
+	else:
+		exit(1)
+
+f.close()
+o.close()
+
+
