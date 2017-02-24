273195d01e7dbd2e9164676d7517a2c6a9377500
mrweber2 <mrweber2@illinois.edu>
2017-02-24 21:26:39 +0000

    fix for VCF format

--- scripts/run_genMut_subsampling.sh
+++ scripts/run_genMut_subsampling.sh
@@ -2,13 +2,13 @@
 
 ###############################################
 #                                             #
-#         run genMutModel on all TSVs         #
+#         run genMutModel on all VCFs         #
 #                                             #
 ###############################################
 
 # for each file ending in .tsv, run genMutModel with CDS only option to produce frequency matrices 
-for file in *.tsv
+for file in *.vcf
 do
-	nohup python ~/neat-genreads/utilities/genMutModel.py -m $file -r ~/oicr/reference/HG19_WholeGenome.fa -co ./matrices/"$file".csv -o ./pickles/"$file".p -bi /shared/HG19_Data/nodecoys_hg19.bed -C BRCA_US --csv > ./logs/progress_"$file".nohup
+	nohup /usr/bin/python ~/neat-genreads/utilities/genMutModel.py -m $file -r ~/references/HG19_WholeGenome.fa -co ./matrices/"$file".csv -o ./pickles/"$file".p -bi ~/references/nodecoys_hg19.bed -C BRCA --csv > ./logs/progress_"$file".nohup
 	echo "$file processed."
 done
